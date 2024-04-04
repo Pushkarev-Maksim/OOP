558df6dc2fd5a22d47196394821a9afd5c1683d2
map39 <map39@W10502>
2024-04-04 16:09:51 +0700

    класс child

--- PersonLibrary/Child.cs
+++ PersonLibrary/Child.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace PersonLibrary
 {
+    /// <summary>
+    /// Класс Child.
+    /// </summary>
     internal class Child : Person
     {
         /// <summary>
